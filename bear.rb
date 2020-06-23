07bbea0ffce579e23a65d5e113fa8b62b4806c08
James Monks <J.monks23@gmail.com>
2020-06-23 19:16:49 +0100

    bear roar pass

--- bear.rb
+++ bear.rb
@@ -2,9 +2,14 @@ class Bear
     attr_reader :name
     attr_accessor :stomach
 
-    def initialize(name)
+    def initialize(name, type)
         @name = name
+        @type = type
         @stomach = []
+    end
+
+    def roar()
+        return "Roar!"
     end
 
 end
--- specs/bear_spec.rb
+++ specs/bear_spec.rb
@@ -5,5 +5,14 @@ Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 require_relative("../bear")
 
 class BearTest < MiniTest::Test
+    
+    def setup()
+    @bear = Bear.new("Yogi", "Grizzly")
+    end
+
+    def test_roar()
+        assert_equal("Roar!", @bear.roar())
+    end
+
 
 end
